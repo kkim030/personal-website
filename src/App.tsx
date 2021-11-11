cc564ce8e7250f47dc039e308e321b72a037ccec
Kelly Kim <kellykim@Kellys-MacBook-Pro.local>
2021-11-11 15:02:54 -0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Kelly Kim
+        </h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Personal Website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
